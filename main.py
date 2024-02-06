0001c4eacd7d23bb36a1e17203add496f5f0081b
HJFallen <53339786+HJFallen@users.noreply.github.com>
2024-02-06 20:14:49 +0000

    got something working with layouts for grids

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
+
+thermistorsPerModule = 3
+
+
+Builder.load_file("view/test.kv")
 
 def decode_data(data):
   #TO BE IMPLEMENTED LATER
   return data
 
+class Start(Screen):
+  thermistorsPerModule = 3
+
+  def add_buttons(self):
+    new_btn = Button(text="hello")
+    self.ids.grid_id.add_widget(new)
 class MyApp(App):
+  
   def build(self):
-    return Label(text="Hello world!")
+    return Start()
   
 if __name__ == '__main__':
+  thermistorsPerModule = 3
   MyApp().run()
