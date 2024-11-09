d2a1b9c0500a4a20f7d6647c0279f74538bb6d12
Avdieienko <avdieienko.d@gmail.com>
2024-11-09 10:33:25 +0000

    More docstrings
    Clean-up

--- source/utils/data_logger.py
+++ source/utils/data_logger.py
@@ -1,17 +1,43 @@
+"""DataLogger module."""
+
 from typing import List
 from time import sleep, time
 from datetime import datetime
+
 from utils.file_writer import write_csv_file
 from utils.constants import MODULE_CSV_FORMAT, THERMISTOR_CSV_FORMAT, MODULE_CSV_ENDING, THERMISTOR_CSV_ENDING, LOGGING_INTERVAL, LOGGING_START
 
 class DataLogger:
-  moduleData: List[str] = [MODULE_CSV_FORMAT]
+  """A class used to represent a DataLogger.
+
+  DataLogger is used to log the data from the modules and thermistors to a file.
+  Note that the data is logged every LOGGING_INTERVAL seconds.
+
+  Attributes:
+    type (str): A string representing the type of the file to write to. Default is "csv".
+    moduleData (List[List[str]]): A list of lists containing the module data. Default is [MODULE_CSV_FORMAT].
+    thermistorData (List[str]): A list of lists containing the thermistor data. Default is [THERMISTOR_CSV_FORMAT].
+    path_identifier (str): A string representing the path identifier. Default is the current date and time.
+  """
+  type: str = "csv"
+  moduleData: List[List[str]] = [MODULE_CSV_FORMAT]
   thermistorData: List[str] = [THERMISTOR_CSV_FORMAT]
   path_identifier: str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-  def __init__(self, type):
+  def __init__(self, type="csv"):
+    """Initialises the DataLogger instance.
+
+    Args:
+        type (str): A string representing the type of the file to write to. Default is "csv".
+    """
     self.type = type
 
   def logging_thread(self, modules, get_app_quit):
+    """The logging thread function.
+
+    Args:
+        modules (_type_): Modules to log data from.
+        get_app_quit (_type_): Function to get the app quit status. Go to the main.py file to see the implementation.
+    """
     start = time()
     while(not get_app_quit()):
       sleep(0.5)
@@ -25,6 +51,11 @@ class DataLogger:
     self.stop_logging()
 
   def stop_logging(self):
+    """Stop the logging process.
+
+    This function is called when the logging process is stopped.
+    Stop the logging process and write the data to the file.
+    """
     if self.type == "csv":
       print("Writing to CSV files")
       write_csv_file(self.moduleData, self.path_identifier + MODULE_CSV_ENDING)
--- source/utils/file_writer.py
+++ source/utils/file_writer.py
@@ -1,16 +1,32 @@
+"""This module contains functions for writing to files
+
+  Example:
+    To write to a file, use the write_csv_file function:
+      write_csv_file([["Column1", "Columns2"],["Value1", "Value2"],...,["Value_n-1", "Value_n"]], "example.csv")
+"""
+
 import csv
 import os
+from typing import List
 
 from utils.constants import LOG_FOLDER
 
+# Get the log folder
 dir = os.path.dirname(__file__)
 log_folder = folder = os.path.join(dir,"../../" ,LOG_FOLDER)
 
 def check_log_dir_exists():
+  """Check if the log folder exists, if not create it"""
   if not os.path.exists(log_folder):
     os.makedirs(log_folder)
 
-def write_csv_file(content, file_path):
+def write_csv_file(content:List[List[str]], file_path: str):
+  """Write content to a csv file
+
+  Args:
+      content (List[List[str]]): The content to write to the file
+      file_path (str): Name of the file to write to
+  """
   check_log_dir_exists()
   file_path = os.path.join(log_folder, file_path)
   with open(file_path, mode='w', newline='') as file:
